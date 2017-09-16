55c9ae6553c5f910b7bebae03f2fb02a940bec74
dao huy tuan <tuandh95@gmail.com>
2017-09-16 22:09:44 +0700

    event next in stepprogess

--- js/stepProgess.js
+++ js/stepProgess.js
@@ -1,3 +1,13 @@
-$(".stepItem:first").addClass("active");
+
+let currentStep = $(".stepItem.active");
+let nextStep = $(".stepItem.active").next().index();
+$(".next").click(function() {
+    
+    
+    $(".stepProgess div").eq(nextStep).addClass("active");
+    nextStep = $(".stepProgess div").eq(nextStep).index() +1;
+    alert(currentStep);
+    
+})
 
 
