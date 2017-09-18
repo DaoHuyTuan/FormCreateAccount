9ad6b1fd8b034c1b0e10b23cbc7a3b73eb568482
dao huy tuan <tuandh95@gmail.com>
2017-09-18 11:57:39 +0700

    next + back for stepProgess

--- js/stepProgess.js
+++ js/stepProgess.js
@@ -1,13 +1,15 @@
 
 let currentStep = $(".stepItem.active");
 let nextStep = $(".stepItem.active").next().index();
+let backStep = $(".stepItem.active").prev().index();
 $(".next").click(function() {
-    
-    
     $(".stepProgess div").eq(nextStep).addClass("active");
     nextStep = $(".stepProgess div").eq(nextStep).index() +1;
-    alert(currentStep);
-    
+    alert(currentStep); 
+});
+$(".back").click(function() {
+    $(".stepProgess div").eq(backStep).removeClass("active");
+    backStep = $(".stepProgess div").eq(backStep).index() -1;
+    alert(currentStep); 
 })
-
 
