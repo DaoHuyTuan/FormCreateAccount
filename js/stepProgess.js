f5140034d1ec723b12fe24964d10b9e5d4a6c48b
dao huy tuan <tuandh95@gmail.com>
2017-09-19 09:03:26 +0700

    DONE

--- js/stepProgess.js
+++ js/stepProgess.js
@@ -2,10 +2,10 @@
 var current_fs , next_fs , prev_fs ;
 $(".next").click(function()  {
     current_fs = $(this).parent();
-    
     next_fs = $(this).parent().next();
     current_fs.hide();
     next_fs.show();
+    $("")
 
     //stepProgess change
  
